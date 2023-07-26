d844392aa7a87a40ecdf9f21a377eec628a6178e
Rodrigo Leon <leonrrodrigo@hotmail.com>
2023-07-26 16:03:11 -0600

    Control input using FormData

--- class05-buscador-peliculas/src/App.jsx
+++ class05-buscador-peliculas/src/App.jsx
@@ -4,12 +4,23 @@ import { Movies } from './components/Movies.jsx';
 
 function App() {
   const { movies: mappedMovies } = useMovies();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const { query } = Object.fromEntries(new window.FormData(event.target));
+
+    console.log(query);
+  };
   return (
     <div className='page'>
       <header>
         <h1>Buscador de peliculas</h1>
-        <form className='form'>
-          <input type='text' placeholder='Avenges,Star Wars, The Matrix' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            name='query'
+            type='text'
+            placeholder='Avenges,Star Wars, The Matrix'
+          />
           <button type='submit'>Buscar</button>
         </form>
       </header>

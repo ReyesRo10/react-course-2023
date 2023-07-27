7c894794a5b5bc59b5effdf5b65ea8771de9d55f
Rodrigo Leon <leonrrodrigo@hotmail.com>
2023-07-27 10:35:59 -0600

    Fetching data

--- class05-buscador-peliculas/src/hooks/useMovies.js
+++ class05-buscador-peliculas/src/hooks/useMovies.js
@@ -1,7 +1,9 @@
-import responseMovies from '../mocks/with-results.json';
+import withResults from '../mocks/with-results.json';
 import withoutResults from '../mocks/no-results.json';
+import { useState } from 'react';
 
-export function useMovies() {
+export function useMovies({ search }) {
+  const [responseMovies, setResponseMovies] = useState([]);
   const movies = responseMovies.Search;
   const mappedMovies = movies?.map((movie) => ({
     id: movie.imdbID,
@@ -9,5 +11,16 @@ export function useMovies() {
     year: movie.Year,
     poster: movie.Poster,
   }));
-  return { movies: mappedMovies };
+
+  const getMovies = () => {
+    if (search) {
+      //setResponseMovies(withResults);
+      fetch(`https://www.omdbapi.com/?s=${search}&apikey=23350a72`)
+        .then((res) => res.json())
+        .then((data) => setResponseMovies(data));
+    } else {
+      setResponseMovies(withoutResults);
+    }
+  };
+  return { movies: mappedMovies, getMovies };
 }
